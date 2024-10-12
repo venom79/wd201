ce4e60c8c06d538bb12d3a45ec963e733926f2a8
venom79 <aditya.gaonkar1907@gmail.com>
2024-10-12 11:08:16 +0530

    add lint-staged

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -1,14 +1,9 @@
-export default {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: 'eslint:recommended',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {},
-};
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+];
