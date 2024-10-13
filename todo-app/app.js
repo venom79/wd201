a5106c993848baa3c20493801d1db3e89791d4da
venom79 <aditya.gaonkar1907@gmail.com>
2024-10-13 19:50:01 +0530

    Prod script upgrade and added ejs

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const path = require("path");
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.set("view engine","ejs");
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get("/", async (request, response)=>{
+  const allTodos = await Todo.getTodos();
+  if(request.accepts('html')){
+    response.render("index",{
+      allTodos,
+    })
+  }
+  else{
+    response.json({
+      allTodos,
+    })
+  }
 });
 
 app.get("/todos", async function (_request, response) {
